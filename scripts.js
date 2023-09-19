fb179145b2b44124955c6ad2bbc53022a2c00804
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-19 18:22:41 +0700

    add feature typing

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const mengetik = new Typed(".typing", {
+    strings: ["Pandhu Arya", "Full Stack Developer", "CEO", "Web Developer", "CTO"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
