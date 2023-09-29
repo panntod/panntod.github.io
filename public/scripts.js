d6fac23d598c87c134a903afed030c5616467dcb
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-29 10:14:15 +0700

    add information name in main

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 const mengetik = new Typed(".typing", {
-    strings: ["Pandhu Arya", "Full Stack Developer", "CEO", "Web Developer", "CTO"],
+    strings: ["Full Stack Developer", "CEO", "Web Developer", "CTO"],
     typeSpeed: 100,
     backSpeed: 50,
     loop: true
