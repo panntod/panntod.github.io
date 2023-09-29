0990d43ca1127167051d80ceb2c309f5694875a6
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-29 10:38:51 +0700

    add DOM to scroll on top button

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,25 @@
 const mengetik = new Typed(".typing", {
-    strings: ["Full Stack Developer", "CEO", "Web Developer", "CTO"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    loop: true
-})
+  strings: ["Full Stack Developer", "CEO", "Web Developer", "CTO"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("scrollToTop");
+  
+    function toggleButtonVisibility() {
+      if (window.scrollY > 50) {
+        button.style.display = "block";
+      } else {
+        button.style.display = "none";
+      }
+    }
+  
+    document.querySelector(".top-button").addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  
+    window.addEventListener("scroll", toggleButtonVisibility);
+  });
+  
