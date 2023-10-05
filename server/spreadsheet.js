9a0c537d4d556d101dbd686e590154f46e501a1f
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-05 12:39:41 +0700

    [refactor] style d-none

--- server/spreadsheet.js
+++ server/spreadsheet.js
@@ -1,24 +1,13 @@
 const scriptURL =
-"https://script.google.com/macros/s/AKfycbxGfA8DJox2zVLcD4JAi5bc9KGMz41oqvxg552--CFf8K9YkbiczLDjQhSW9877zRMBlg/exec";
+"https://script.google.com/macros/library/d/1Uy0aBNJVAbfFbnUo3xBNFAYdUcdKeNJXeTQMbXODU2hKrHbcIWKUAkZu/1";
 
 const form = document.forms["contact-form"];
-const tombolKirim = document.querySelector('.tombol-kirim')
-const tombolLoading = document.querySelector('.tombol-loading')
-const alert = document.querySelector('.alert')
 
 form.addEventListener("submit", (e) => {
 e.preventDefault();
 
-tombolLoading.classList.toggle('d-none')
-tombolKirim.classList.toggle('d-none')
-
 fetch(scriptURL, { method: "POST", body: new FormData(form) })
   .then((response) => {
-    tombolLoading.classList.toggle('d-none')
-    tombolKirim.classList.toggle('d-none')
-    alert.classList.toggle('d-none')
-
-    //reset form
     form.reset()
     console.log("Success!", response)
   })
