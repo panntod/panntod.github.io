488db878d74c42e7da9f2f9fdc4565544b6f1dd7
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-05 16:11:27 +0700

    [fix] link spreadsheet, [doc] added form input in contact

--- server/spreadsheet.js
+++ server/spreadsheet.js
@@ -1,15 +1,25 @@
 const scriptURL =
-"https://script.google.com/macros/library/d/1Uy0aBNJVAbfFbnUo3xBNFAYdUcdKeNJXeTQMbXODU2hKrHbcIWKUAkZu/1";
-
+  "https://script.google.com/macros/s/AKfycbxGfA8DJox2zVLcD4JAi5bc9KGMz41oqvxg552--CFf8K9YkbiczLDjQhSW9877zRMBlg/exec";
 const form = document.forms["contact-form"];
+const tombolKirim = document.querySelector(".tombol-kirim");
+const tombolLoading = document.querySelector(".tombol-loading");
+const alert = document.querySelector(".alert");
 
 form.addEventListener("submit", (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-fetch(scriptURL, { method: "POST", body: new FormData(form) })
-  .then((response) => {
-    form.reset()
-    console.log("Success!", response)
-  })
-  .catch((error) => console.error("Error!", error.message));
+  tombolLoading.classList.toggle("d-none");
+  tombolKirim.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      tombolLoading.classList.toggle("d-none");
+      tombolKirim.classList.toggle("d-none");
+      alert.classList.toggle("d-none");
+
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
+      
