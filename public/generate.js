c5a39548e40c60ae0599bbf175fd9322fbe92bab
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-19 08:33:22 +0700

    [feat] new button figma,[doc] added new project

--- public/generate.js
+++ public/generate.js
@@ -56,8 +56,20 @@ const projectsData = [
         description: "Website ini dibuat untuk memenuhi tugas dari bu ratih, memahami lanjutan tentang javascript.",
         buildWith: "Html5, Css3, Js, Tailwind",
         hasDemo: true,
+        hasFigma: false,
         githubLink: "https://github.com/panntod/Web-Sederhana",
         demoLink: "https://matematika-sederhana.vercel.app/home.html",
+    },
+    {
+        title: "Munjalindra Website",
+        imgSrc: "images/munjalindra-website.png",
+        description: "Website ini dibuat untuk belajar membuat web sesuai dengan figma, dan memperbarui web munjalindra.co.id",
+        buildWith: "Html5, Css3, Js, Boostrap",
+        hasDemo: false,
+        hasFigma: true,
+        githubLink: "https://github.com/panntod/Munjalindra-Web",
+        demoLink: "",
+        figmaLink: "https://www.figma.com/file/WimiUyAlFqnBbd5c3AXFj5/RancanganWeb?type=design&node-id=1%3A4&mode=design&t=ra3edPXnrFHmMAEY-1",
     },
 ];
 
@@ -75,8 +87,9 @@ function generateProjectCard(project) {
             <p class="card__description mg-d-none"><strong>Build with:</strong> ${project.buildWith}</p>
             ${project.hasDemo ? `<p class="card__description">[Sudah dibuat tampilan]</p>` : ""}
             <div class="card__button">
-                <a href="${project.githubLink}" class="github"><i class="bx bxl-github"></i>Github</a>
+                <a href="${project.githubLink}" class="github"><i class="bx bxl-github"></i> Github</a>
                 ${project.hasDemo ? `<a href="${project.demoLink}" class="demo">Demo</a>` : ""}
+                ${project.hasFigma ? `<a href="${project.figmaLink}" class="figma"><i class="bx bxl-figma"></i> Figma</a>` : ""}
             </div>
         </div>
     `;
