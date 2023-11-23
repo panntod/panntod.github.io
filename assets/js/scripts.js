9d77ea0554a604cd4e12bc13f554055a94d9b3d5
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-24 06:39:05 +0700

    [feat] added function darkmode

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,3 +54,21 @@ const mengetik = new Typed(".typing", {
   backSpeed: 50,
   loop: true,
 });
+
+// Ambil tombol dan tambahkan event listener
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.removeItem('darkMode');
+  }
+});
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+  body.classList.add('dark-mode');
+}
