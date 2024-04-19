909a48e0d18c6099b7af02fb8ca46882855f126b
panntod <pandhu.munjalindra@gmail.com>
2024-04-19 19:41:25 +0700

    feat: Deleted unused code navbar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("scrollToTop");
-  const navLinks = document.querySelectorAll(".navbar-nav a");
-  const navbarToggler = document.querySelector(".navbar-toggler");
-  const navbarCollapse = document.querySelector(".navbar-collapse");
-  const hamburger = document.querySelector(".hamburger");
-
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      if (window.innerWidth < 992) {
-        // Check if viewport is mobile (Bootstrap lg breakpoint)
-        navbarToggler.click();
-      }
-    });
-  });
-
-  window.addEventListener("scroll", () => {
-    if (window.innerWidth < 992 && navbarCollapse.classList.contains("show")) {
-      navbarToggler.click(); // Tutup navbar jika sedang terbuka pada saat scroll
-    }
-  });
-
-  function toggleButtonVisibility() {
-    if (window.scrollY > 50) {
-      button.style.display = "block";
-    } else {
-      button.style.display = "none";
-    }
-  }
-
-  function toggleInnerHTML() {
-    const ariaExpandedValue = navbarToggler.getAttribute("aria-expanded");
-    if (ariaExpandedValue === "true") {
-      hamburger.innerHTML = "x"; // Ubah inner HTML jika navbar aktif
-    } else {
-      hamburger.innerHTML = "&#8801;"; // Kembalikan inner HTML jika navbar tidak aktif
-    }
-  }
-
-  navbarToggler.addEventListener("click", () => {
-    toggleInnerHTML();
-  });
-
-  button.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-
-  window.addEventListener("scroll", toggleButtonVisibility);
-});
-
 let mengetik;
 if (typeof Typed === "function") {
   mengetik = new Typed(".typing", {
@@ -64,6 +14,10 @@ if (typeof Typed === "function") {
   });
 } else {
   mengetik = null;
+}
+
+function navigate(path) {
+  window.location.href = "#" + path;
 }
 
 // Ambil tombol dan tambahkan event listener
